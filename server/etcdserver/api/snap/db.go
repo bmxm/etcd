2f600f8e3d2bbd27cecd3de4d7892615a5fd0fb7
wangxu <wangxu@powerlaw.ai>
2023-02-03 15:43:55 +0800

    snap 模块注释

--- server/etcdserver/api/snap/db.go
+++ server/etcdserver/api/snap/db.go
@@ -31,32 +31,44 @@ import (
 
 var ErrNoDBSnapshot = errors.New("snap: snapshot file doesn't exist")
 
+// Snapshotter.SaveDBFrom()方法和 DBFilePath()方法提供了另一套读写快照数据的方式，生成的快照文件名称也与 snapshotter.go 不同。
+
 // SaveDBFrom saves snapshot of the database from the given reader. It
 // guarantees the save operation is atomic.
 func (s *Snapshotter) SaveDBFrom(r io.Reader, id uint64) (int64, error) {
 	start := time.Now()
 
+	// 创建临时文件
 	f, err := ioutil.TempFile(s.dir, "tmp")
 	if err != nil {
 		return 0, err
 	}
+
 	var n int64
+	// 将快照数据写入临时文件中
 	n, err = io.Copy(f, r)
 	if err == nil {
 		fsyncStart := time.Now()
+		// 将临时文件的改动刷新到磁盘
 		err = fileutil.Fsync(f)
 		snapDBFsyncSec.Observe(time.Since(fsyncStart).Seconds())
 	}
+
+	// 关闭临时文件
 	f.Close()
 	if err != nil {
 		os.Remove(f.Name())
 		return n, err
 	}
+
+	// 获取指定的“snap.db”文件，如果存在则将其删除
 	fn := s.dbFilePath(id)
 	if fileutil.Exist(fn) {
 		os.Remove(f.Name())
 		return n, nil
 	}
+
+	// 重命名临时文件
 	err = os.Rename(f.Name(), fn)
 	if err != nil {
 		os.Remove(f.Name())
@@ -77,9 +89,12 @@ func (s *Snapshotter) SaveDBFrom(r io.Reader, id uint64) (int64, error) {
 // DBFilePath returns the file path for the snapshot of the database with
 // given id. If the snapshot does not exist, it returns error.
 func (s *Snapshotter) DBFilePath(id uint64) (string, error) {
+	// 尝试读取快照目录，主要是检测快照文件是否存在
 	if _, err := fileutil.ReadDir(s.dir); err != nil {
 		return "", err
 	}
+
+	// 获取指定的快照db文件的绝对路径
 	fn := s.dbFilePath(id)
 	if fileutil.Exist(fn) {
 		return fn, nil
@@ -96,5 +111,6 @@ func (s *Snapshotter) DBFilePath(id uint64) (string, error) {
 }
 
 func (s *Snapshotter) dbFilePath(id uint64) string {
+	// 生成的快照文件的名称结构
 	return filepath.Join(s.dir, fmt.Sprintf("%016x.snap.db", id))
 }
--- server/etcdserver/api/snap/snapshotter.go
+++ server/etcdserver/api/snap/snapshotter.go
@@ -50,8 +50,19 @@ var (
 	}
 )
 
+// 随着节点的运行，会处理客户端和集群中其他节点发来的大量请求，相应的 WAL 日志量会不断增加，会产生大量的WAL日志文件，
+// 另外 etcd-raft 模块中的 raftLog 中也会存储大量的 Entry 记录，这就会导致资源浪费。
+// 当节点宕机之后，如果要恢复其状态，则需要从头读取全部的WAL日志文件，这显然是非常耗时的。
+// 为了解决这些问题，etcd 会定期创建快照并将其保存到本地磁盘中，在恢复节点状态时会先加载快照文件，使用该快照数据将节点恢复到对应的状态，
+// 之后从快照数据之后的相应位置开始读取WAL日志文件，最终将节点恢复到正确的状态。
+
+// 与WAL日志的管理类似，etcd 并未在 etcd-raft 模块中实现将快照管理，而是将其独立到了snap 模块中。
+// 其中 SnapShotter 通过文件的方式管理快照数据，它是 snapshot 模块的核心。
+
 type Snapshotter struct {
-	lg  *zap.Logger
+	lg *zap.Logger
+
+	// 指定了存储快照文件的目录位置。
 	dir string
 }
 
@@ -75,10 +86,19 @@ func (s *Snapshotter) SaveSnap(snapshot raftpb.Snapshot) error {
 func (s *Snapshotter) save(snapshot *raftpb.Snapshot) error {
 	start := time.Now()
 
+	// 创建快照文件名，快照文件的名称由三部分组成，分别是快照所涵盖的最后一条Entry记录的Term、Index和“.snap”后缀
 	fname := fmt.Sprintf("%016x-%016x%s", snapshot.Metadata.Term, snapshot.Metadata.Index, snapSuffix)
+
 	b := pbutil.MustMarshal(snapshot)
 	crc := crc32.Update(0, crcTable, b)
+
+	// 将序列化后的数据和校验码封装成 snappb.Snapshot 实例，
+	// 这里简单了解一下 raftpb.Snapshot 和 snappb.Snapshot 的区别，
+	// 前者包含了 SnapShot 数据及一些元数据（例如，该快照数据所涵盖的最后一条Entry记录的Term和Index）；
+	// 后者则是在前者序列化之后的封装，其中还记录了相应的校验码等信息
 	snap := snappb.Snapshot{Crc: crc, Data: b}
+
+	// 将快照数据进行序列化
 	d, err := snap.Marshal()
 	if err != nil {
 		return err
@@ -88,6 +108,7 @@ func (s *Snapshotter) save(snapshot *raftpb.Snapshot) error {
 	spath := filepath.Join(s.dir, fname)
 
 	fsyncStart := time.Now()
+	// 将snappb.Snapshot序列化后的数据写入文件，并同步刷新到磁盘
 	err = pioutil.WriteAndSyncFile(spath, d, 0666)
 	snapFsyncSec.Observe(time.Since(fsyncStart).Seconds())
 
@@ -105,6 +126,8 @@ func (s *Snapshotter) save(snapshot *raftpb.Snapshot) error {
 }
 
 // Load returns the newest snapshot.
+// 该方法会读取指定目录下的全部快照文件，并查找其中最近的可用快照文件，
+// 然后通过 Snapshotter.loadSnap() 方法加载其中的快照数据
 func (s *Snapshotter) Load() (*raftpb.Snapshot, error) {
 	return s.loadMatching(func(*raftpb.Snapshot) bool { return true })
 }
@@ -124,12 +147,16 @@ func (s *Snapshotter) LoadNewestAvailable(walSnaps []walpb.Snapshot) (*raftpb.Sn
 
 // loadMatching returns the newest snapshot where matchFn returns true.
 func (s *Snapshotter) loadMatching(matchFn func(*raftpb.Snapshot) bool) (*raftpb.Snapshot, error) {
+	// 获取全部快照文件的名称，其中会检查文件的后缀名，过滤不合法的文件
 	names, err := s.snapNames()
 	if err != nil {
 		return nil, err
 	}
+
 	var snap *raftpb.Snapshot
 	for _, name := range names {
+		// 按序读取快照文件，直到读取到第一个合法的快照文件，对于读取失败的快照文件则添加“.broken”后缀，
+		// 这样在下次调用snapNames()方法获取可用快照文件时，就会将其过滤掉
 		if snap, err = loadSnap(s.lg, s.dir, name); err == nil && matchFn(snap) {
 			return snap, nil
 		}
@@ -138,9 +165,13 @@ func (s *Snapshotter) loadMatching(matchFn func(*raftpb.Snapshot) bool) (*raftpb
 }
 
 func loadSnap(lg *zap.Logger, dir, name string) (*raftpb.Snapshot, error) {
+	// 获取快照文件的绝对路径
 	fpath := filepath.Join(dir, name)
+
+	// 读取快照文件的内容
 	snap, err := Read(lg, fpath)
 	if err != nil {
+		// 如果读取失败，则将该快照文件的后缀名修改成“.broken”
 		brokenPath := fpath + ".broken"
 		if lg != nil {
 			lg.Warn("failed to read a snap file", zap.String("path", fpath), zap.Error(err))
@@ -176,6 +207,7 @@ func Read(lg *zap.Logger, snapname string) (*raftpb.Snapshot, error) {
 	}
 
 	var serializedSnap snappb.Snapshot
+	// 将读取到的数据进行反序列化
 	if err = serializedSnap.Unmarshal(b); err != nil {
 		if lg != nil {
 			lg.Warn("failed to unmarshal snappb.Snapshot", zap.String("path", snapname), zap.Error(err))
@@ -190,6 +222,7 @@ func Read(lg *zap.Logger, snapname string) (*raftpb.Snapshot, error) {
 		return nil, ErrEmptySnapshot
 	}
 
+	// 使用 crc32 校验码验证数据是否正确
 	crc := crc32.Update(0, crcTable, serializedSnap.Data)
 	if crc != serializedSnap.Crc {
 		if lg != nil {
@@ -203,6 +236,7 @@ func Read(lg *zap.Logger, snapname string) (*raftpb.Snapshot, error) {
 	}
 
 	var snap raftpb.Snapshot
+	// 反序列化 snappb.Snapsho t实例的 Data 字段，得到 raftpb.Snapshot 实例
 	if err = snap.Unmarshal(serializedSnap.Data); err != nil {
 		if lg != nil {
 			lg.Warn("failed to unmarshal raftpb.Snapshot", zap.String("path", snapname), zap.Error(err))
@@ -232,6 +266,8 @@ func (s *Snapshotter) snapNames() ([]string, error) {
 	if len(snaps) == 0 {
 		return nil, ErrNoSnapshot
 	}
+
+	// 排序
 	sort.Sort(sort.Reverse(sort.StringSlice(snaps)))
 	return snaps, nil
 }
